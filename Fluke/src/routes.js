f84473ab436ded75b8e6ec3b9d7f3d9c0bb49028
Natalia <natyap28@hotmail.com>
2021-06-19 23:01:46 -0300

    add tab navigator

--- Fluke/src/routes.js
+++ Fluke/src/routes.js
@@ -1,16 +1,65 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {HomeScreen} from './pages/first';
+// import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
+Icon.loadFont();
+import Home from './pages/Home';
+import FAQ from './pages/FAQ';
+import DataUsage from './pages/DataUsage';
+import AdditionalPlans from './pages/AdditionalPlans';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: '#0ef500',
+          inactiveTintColor: '#b5b5b5',
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => (
+              <Icon name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="DataUsage"
+          component={DataUsage}
+          options={{
+            tabBarLabel: 'Histórico',
+            tabBarIcon: ({color}) => (
+              <Icon name="bar-chart-2" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="AdditionalPlans"
+          component={AdditionalPlans}
+          options={{
+            tabBarLabel: 'Pacotes',
+            tabBarIcon: ({color}) => (
+              <Icon name="plus-circle" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="FAQ"
+          component={FAQ}
+          options={{
+            tabBarLabel: 'FAQ',
+            tabBarIcon: ({color}) => (
+              <Icon name="help-circle" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
